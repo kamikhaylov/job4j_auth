6325fb7b1b83caba1a41cb6e0a1b31b17c2c47ac
konstantin <kotmix@bk.ru>
2023-10-05 01:30:56 +0400

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/Job4jAuthApplication.java
@@ -2,9 +2,16 @@ package ru.job4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Job4jAuthApplication {
+public class Job4jAuthApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Job4jAuthApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Job4jAuthApplication.class, args);
